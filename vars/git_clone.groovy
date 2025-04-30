5a8f4d583e689d8429b935681d9d832ad5298241
rajendrakmr <48769664+rajendrakmr@users.noreply.github.com>
2025-04-30 10:05:19 +0530

    Update git_clone.groovy

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,2 +1,4 @@
-def(String crendentialId){
-withCredential[
+def(String crendentialId,String url,String branch){
+withCredentials[usernamePassword(credentialsId:'crendentialId',usernameVariable:'GIT_USERNAME',passwordVariable:'GIT_TOKEN')]{
+  git url: "${url}", branch: "${branch}"
+}}
