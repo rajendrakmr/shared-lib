e9d9cc055f29673cd48f4eddf7d6a235f36ee262
Rajendra Kumar Marandi <48769664+rajendrakmr@users.noreply.github.com>
2025-08-29 15:23:47 +0530

    Update code_analysis.groovy

--- var/code_analysis.groovy
+++ var/code_analysis.groovy
@@ -1,1 +1,13 @@
 
+def call(){
+  withSonarQubeEnv('Sonar') {
+          sh '''
+            sonar-scanner \
+              -Dsonar.projectKey=school-management-system \
+              -Dsonar.projectName=school-management-system \
+              -Dsonar.sources=. \
+              -Dsonar.java.binaries=. \
+              -X
+          '''
+        }
+}
